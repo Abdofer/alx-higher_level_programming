2d239f3cfa5412052848251dd97f0cca4e95abd8
Abdofer <abderrazaq.fermaq@gmail.com>
2023-06-27 17:23:43 -0700

    class area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" define a class Square."""
+""" define a class Square. """
 
 
 class Square():
@@ -14,4 +14,3 @@ class Square():
 
     def area(self):
         return (self.__size * self.__size)
-
